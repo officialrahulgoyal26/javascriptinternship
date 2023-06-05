5a5190efd14419f801b3cbcb59ad6fb3d3ab58e2
officialrahulgoyal26 <officialrahulgoyal26@gmail.com>
2023-06-05 17:52:39 +0530

    next code

--- 05.js
+++ 05.js
@@ -4,3 +4,8 @@
 // const myName = "Rahul";
 // const myName = "shivam";(this way is wrong)
 // console.log(myName)
+ 
+// but this is right
+const myName="rahul"
+console.log(myName)
+// note : you can not change the value of const name when it already have 
