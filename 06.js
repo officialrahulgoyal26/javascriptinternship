1c32e95b1f1dfbedbeee6bfd6017e7d7bdaf2115
officialrahulgoyal26 <officialrahulgoyal26@gmail.com>
2023-06-06 10:30:08 +0530

    string indexing

---
+++ 06.js
@@ -0,0 +1,11 @@
+// string indexing
+// let firstName = "Rahul";
+// console.log( firstName[2]);
+
+// string length 
+// let firstName = "rahul"
+// console.log(firstName.length)
+// (note if you include space in name then length also include space in output )
+
+let firstName = "rahul "
+console.log(firstName.length)
