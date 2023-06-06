fc2f00ac76574545de0e1e3317ac7745b99a3af1
officialrahulgoyal26 <officialrahulgoyal26@gmail.com>
2023-06-06 10:35:47 +0530

    update

--- 06.js
+++ 06.js
@@ -7,5 +7,9 @@
 // console.log(firstName.length)
 // (note if you include space in name then length also include space in output )
 
-let firstName = "rahul "
-console.log(firstName.length)
+// let firstName = "rahul "
+// console.log(firstName.length)
+
+// last index 
+let firstName="rahulgoyal"
+console.log(firstName[firstName.length-5])
